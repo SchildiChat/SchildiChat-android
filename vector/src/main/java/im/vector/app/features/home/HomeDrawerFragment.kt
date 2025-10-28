7560dc9290ebcde5357e21b1140c4bdd8e617494
Sharon Avni <sharon@pelanor.io>
2025-10-28 17:13:16 +0200

    Disable Logout button

--- vector/src/main/java/im/vector/app/features/home/HomeDrawerFragment.kt
+++ vector/src/main/java/im/vector/app/features/home/HomeDrawerFragment.kt
@@ -79,11 +79,6 @@ class HomeDrawerFragment :
             sharedActionViewModel.post(HomeActivitySharedAction.CloseDrawer)
             navigator.openSettings(requireActivity())
         }
-        // Sign out
-        views.homeDrawerHeaderSignoutView.debouncedClicks {
-            sharedActionViewModel.post(HomeActivitySharedAction.CloseDrawer)
-            SignOutUiWorker(requireActivity()).perform()
-        }
 
         views.homeDrawerQRCodeButton.debouncedClicks {
             UserCodeActivity.newIntent(requireContext(), sharedActionViewModel.session.myUserId).let {
